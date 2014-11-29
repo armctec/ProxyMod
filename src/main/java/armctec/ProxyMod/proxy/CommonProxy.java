fb9175318517599a8736b1e379fce665502d12b7
armctec <armctec@gmail.com>
2014-11-29 19:31:03 -0200

    Corrigido, adicionado registro da tile entity

--- src/main/java/armctec/ProxyMod/proxy/CommonProxy.java
+++ src/main/java/armctec/ProxyMod/proxy/CommonProxy.java
@@ -1,7 +1,7 @@
 package armctec.ProxyMod.proxy;
 
-import armctec.ProxyMod.compat.CCProvider;
 import armctec.ProxyMod.compat.MFRProvider;
+import armctec.ProxyMod.integration.CCProvider;
 import armctec.ProxyMod.reference.Names;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -9,7 +9,8 @@ public abstract class CommonProxy implements IProxy
 {
     public void registerTileEntities()
     {
-        GameRegistry.registerTileEntity(CCProvider.class, Names.Blocks.BUNDLEDCC);
-        GameRegistry.registerTileEntity(MFRProvider.class, Names.Blocks.REDNETPROXY);
+        //GameRegistry.registerTileEntity(CCProvider.class, Names.Blocks.BUNDLEDCC);
+        //GameRegistry.registerTileEntity(MFRProvider.class, Names.Blocks.REDNETPROXY);
+        GameRegistry.registerTileEntity(CCProvider.class, Names.Blocks.PROXYCCMFR);
     }
 }
